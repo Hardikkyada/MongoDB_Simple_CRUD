5b62bbdf2e5f2b4e1c2b1ac36849b85425d75e84
Hardikkyada <81248575+Hardikkyada@users.noreply.github.com>
2021-12-06 18:11:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require('express')
 const app = express()
 app.use(express.json());
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8082;
 const mongoose = require("mongoose");
 
 
